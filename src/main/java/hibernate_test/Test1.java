7cad4d2911de255c5138f677c7de640ba9f08364
VladimirKing <cheremuha1@gmail.com>
2021-03-06 14:26:42 +0200

    m

--- src/main/java/hibernate_test/Test1.java
+++ src/main/java/hibernate_test/Test1.java
@@ -1,2 +1,14 @@
-package hibernate_test.adres;public class Test1 {
+package hibernate_test;
+
+import hibernate_test.adres.Address;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test1 {
+    public static void main(String[] args) {
+
+
+
+    }
 }
