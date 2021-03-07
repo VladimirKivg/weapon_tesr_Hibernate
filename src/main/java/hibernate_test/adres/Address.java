ad40b5f082e35a106ea492c34a5a08491ece2e16
VladimirKing <cheremuha1@gmail.com>
2021-03-07 07:38:41 +0200

    mm

--- src/main/java/hibernate_test/adres/Address.java
+++ src/main/java/hibernate_test/adres/Address.java
@@ -1,14 +1,12 @@
 package hibernate_test.adres;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "adress")
 public class Address {
     @Id
+            @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
     @Column(name = "city")
     String city;
