62569618736ad0405a60907235c214b0e352f1a6
VladimirKing <cheremuha1@gmail.com>
2021-03-07 23:06:49 +0200

    mm

--- src/main/java/hibernate_test/Test3.java
+++ src/main/java/hibernate_test/Test3.java
@@ -1,5 +1,5 @@
 package hibernate_test;
-// дастаем один обект
+// изменяем данные обектов
 import hibernate_test.adres.Address;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -10,25 +10,40 @@ import java.util.List;
 public class Test3 {
     public static void main(String[] args) {
 
-        SessionFactory factory = new Configuration()
+//lesson1();//дістаємо один обєкт і робим в ньому зміни
+
+        SessionFactory factory =new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Address.class)
                 .buildSessionFactory();
         try {
-Session session = factory.getCurrentSession();
-session.beginTransaction();
-            List<Address> addressList =session
-                    .createQuery("from Address")
-                    .getResultList();
- session.getTransaction();
-            for (Address address: addressList) {
-                System.out.println(address);
-            }
 
 
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+            session.createQuery("update Address set flet_nember=22 " +
+                    "where city='Чернівці' ").executeUpdate();
+            session.getTransaction().commit();
         } finally {
             factory.close();
         }
 
+    }
+
+    private static void lesson1() {
+        SessionFactory factory =new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Address.class)
+                .buildSessionFactory();
+        try {
+
+            Session session = factory.getCurrentSession();
+session.beginTransaction();
+Address address=session.get(Address.class,2);
+address.setCity("франківськ");
+session.getTransaction().commit();
+        } finally {
+            factory.close();
+        }
     }
 }
