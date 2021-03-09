e14ebed24b4826f636c60fb731fbc4cda74ab224
VladimirKing <cheremuha1@gmail.com>
2021-03-09 19:03:33 +0200

    m

--- src/main/java/hibernate_test/Test4.java
+++ src/main/java/hibernate_test/Test4.java
@@ -1,4 +1,33 @@
 package hibernate_test;
 
+import hibernate_test.adres.Address;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Test4 {
+    public static void main(String[] args) {
+//lesson1(); // видаляєм один обект за id
+
+
+    }
+
+    private static void lesson1() {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Address.class)
+                .buildSessionFactory();
+        try {
+
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+Address address = session.get(Address.class,3);
+session.delete(address);
+            session.getTransaction().commit();
+        } catch (Exception e){
+            System.out.println("спіймали помилку");
+        } finally {
+            factory.close();
+        }
+    }
 }
