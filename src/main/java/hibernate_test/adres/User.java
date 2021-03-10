1a85af48b31ded7847f487fcdd7d0991bdff209e
VladimirKing <cheremuha1@gmail.com>
2021-03-10 19:28:21 +0200

    mm

--- src/main/java/hibernate_test/adres/User.java
+++ src/main/java/hibernate_test/adres/User.java
@@ -1,6 +1,15 @@
 package hibernate_test.adres;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @Column(name = "id")
     private int id;
     private String name;
     private int addressId;
