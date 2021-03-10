365e8ca8dc37d29febceba2e782c4b5fd67827be
VladimirKing <cheremuha1@gmail.com>
2021-03-10 19:35:06 +0200

    mm

--- src/main/java/hibernate_test/adres/User.java
+++ src/main/java/hibernate_test/adres/User.java
@@ -1,9 +1,6 @@
 package hibernate_test.adres;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
@@ -11,6 +8,50 @@ public class User {
     @Id
     @Column(name = "id")
     private int id;
+    @Column(name = "name")
     private String name;
-    private int addressId;
+     @OneToOne
+     @JoinColumn(name = "adress_id")
+    private Address addressId;
+
+    public User() {
+    }
+
+    public User(String name, Address addressId) {
+        this.name = name;
+        this.addressId = addressId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddressId() {
+        return addressId;
+    }
+
+    public void setAddressId(Address addressId) {
+        this.addressId = addressId;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", addressId=" + addressId +
+                '}';
+    }
 }
