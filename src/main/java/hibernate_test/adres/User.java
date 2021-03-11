1b43d0fe587b26e9b682d97383ce1bee4e1db4e9
VladimirKing <cheremuha1@gmail.com>
2021-03-11 10:46:19 +0200

    mm

--- src/main/java/hibernate_test/adres/User.java
+++ src/main/java/hibernate_test/adres/User.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 @Table(name = "user")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private int id;
     @Column(name = "name")
     private String name;
