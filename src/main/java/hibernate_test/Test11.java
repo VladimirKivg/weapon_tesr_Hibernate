1d6e3a970ddf37e8affa5d5982f177f0d0bd8bf5
VladimirKing <cheremuha1@gmail.com>
2021-03-11 08:37:15 +0200

    m

--- src/main/java/hibernate_test/Test11.java
+++ src/main/java/hibernate_test/Test11.java
@@ -1,2 +1,29 @@
-package hibernate_test;public class Test11 {
+package hibernate_test;
+
+import hibernate_test.adres.Address;
+import hibernate_test.adres.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test11 {
+    public static void main(String[] args) {
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .addAnnotatedClass(Address.class)
+                .buildSessionFactory();
+
+
+
+        Session session = factory.getCurrentSession();
+
+        Address address = new Address("Чернівці","бульва Героїв Крут",20,41);
+        User user = new User("Вован",address);
+        session.beginTransaction();
+
+
+        session.getTransaction().commit();
+
+    }
 }
