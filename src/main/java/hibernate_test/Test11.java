aca26b7fcf88b379e4cc9031cfdb78891b841c39
VladimirKing <cheremuha1@gmail.com>
2021-03-13 00:08:18 +0200

    m

--- src/main/java/hibernate_test/Test11.java
+++ src/main/java/hibernate_test/Test11.java
@@ -2,11 +2,12 @@ package hibernate_test;
 
 import hibernate_test.adres.Address;
 import hibernate_test.adres.User;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test11 {
+public class Test11 {// записеєм обєкт в базу данних
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -21,11 +22,14 @@ public class Test11 {
             Session session = factory.getCurrentSession();
 
             Address address = new Address("Чернівці", "бульва Героїв Крут", 20, 41);
-            User user = new User("Вованчик", address);
+            User user = new User("Вованчик Хулиганчик", address);
+
             session.beginTransaction();
             session.save(user);
 
             session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.out.println("ловим ошибку");
         } finally {
             factory.close();
         }
