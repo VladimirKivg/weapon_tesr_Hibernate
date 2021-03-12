c76a260cc9ad64616c527ded2b24163c3eb24461
VladimirKing <cheremuha1@gmail.com>
2021-03-13 00:09:32 +0200

    mm

--- src/main/java/hibernate_test/Test13.java
+++ src/main/java/hibernate_test/Test13.java
@@ -1,2 +1,43 @@
-package hibernate_test;public class Test13 {
+package hibernate_test;
+
+import hibernate_test.adres.Address;
+import hibernate_test.adres.User;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test13 {
+    //знаходимо да видалямо обєк використовуючи зворотній звязок
+
+    static SessionFactory factory;
+
+    static {
+        factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Address.class)
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        getObject(); // дістаємо обект
+    }
+
+    private static void getObject() {
+        Session session=null;
+        try {
+            session = factory.openSession();
+            session.beginTransaction();
+Address address =session.get(Address.class,12);
+            System.out.println(address.getUser());// ось тут ховається весь прікол
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            System.out.println("ловим помилку");
+            e.printStackTrace();
+        } finally {
+            factory.close();
+session.close();
+        }
+    }
 }
