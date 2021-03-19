969d22132c9f63bb658da1bb94e2a7513bb470fe
VladimirKing <cheremuha1@gmail.com>
2021-03-20 00:58:22 +0200

    mm

--- src/main/java/hibirnate_test3/Test31.java
+++ src/main/java/hibirnate_test3/Test31.java
@@ -1,2 +1,26 @@
-package hibirnate_test3;public class Test31 {
+package hibirnate_test3;
+
+import hibirnate_test3.manyToMany.Agency;
+import hibirnate_test3.manyToMany.Apartment;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test31 {
+    static SessionFactory factory;
+    {factory= new  Configuration()
+            .configure("hibernate.cfg.xml")
+            .addAnnotatedClass(Agency.class)
+            .addAnnotatedClass(Apartment.class)
+            .buildSessionFactory();
+    }
+
+    public static void main(String[] args) {
+        Session session=null;
+        try {
+            session=factory.getCurrentSession();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
