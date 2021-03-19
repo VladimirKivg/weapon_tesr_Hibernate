974ea9f630c20f6cdbd2b90385c57d8566f10983
VladimirKing <cheremuha1@gmail.com>
2021-03-20 00:57:04 +0200

    mm

--- src/main/java/hibirnate_test3/manyToMany/Apartment.java
+++ src/main/java/hibirnate_test3/manyToMany/Apartment.java
@@ -1,2 +1,94 @@
-package hibirnate_test3.manyToMany;public class Apartment {
+package hibirnate_test3.manyToMany;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+
+@Entity
+@Table(name = "apartment")
+public class Apartment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "city")
+    private String city;
+
+    @Column(name = "strait")
+    private String strait;
+
+    @Column(name = "house")
+    private Integer house;
+
+    @JoinTable(name = "agency_and_apartment",
+    joinColumns = @JoinColumn(name = "id_apartm"),
+    inverseJoinColumns = @JoinColumn(name = "id_agent"))
+ArrayList<Agency>agen;
+
+    public void addAgency(Agency agency){
+        if (agen==null){
+            agen=new ArrayList<>();
+        }
+        agen.add(agency);
+    }
+
+    public Apartment() {
+    }
+
+    public Apartment(String city, String strait, Integer house) {
+        this.city = city;
+        this.strait = strait;
+        this.house = house;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStrait() {
+        return strait;
+    }
+
+    public void setStrait(String strait) {
+        this.strait = strait;
+    }
+
+    public Integer getHouse() {
+        return house;
+    }
+
+    public void setHouse(Integer house) {
+        this.house = house;
+    }
+
+    public ArrayList<Agency> getAgen() {
+        return agen;
+    }
+
+    public void setAgen(ArrayList<Agency> agen) {
+        this.agen = agen;
+    }
+
+    @Override
+    public String toString() {
+        return "Apartment{" +
+                "id=" + id +
+                ", city='" + city + '\'' +
+                ", strait='" + strait + '\'' +
+                ", house=" + house +
+                '}';
+    }
 }
