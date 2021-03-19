e179157883ca7f84f8a811e7c550cf36f88035b7
VladimirKing <cheremuha1@gmail.com>
2021-03-20 00:59:50 +0200

    mm

--- src/main/java/hibernate_test2/oneToMany/uni/Address3.java
+++ src/main/java/hibernate_test2/oneToMany/uni/Address3.java
@@ -1,14 +1,12 @@
 package hibernate_test2.oneToMany.uni;
 
-import hibernate_test2.oneToMany.User2;
-
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "adress")
-public class Address2 {
+public class Address3 {
     /*id
     * city
     * strit
@@ -18,30 +16,32 @@ public class Address2 {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private int id;
+    private Integer id;
     @Column(name = "city")
     private String city;
     @Column(name = "strit")
-    private String street;
+
+     private String street;
     @Column(name = "house_number")
     private int houseNumber;
     @Column(name = "flet_nember")
-    private int apartmentNumber;
-    @OneToMany(cascade = CascadeType.ALL,mappedBy = "addressId")
-private List<User2>user2s;
+    private Integer apartmentNumber;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "adress_id")
+private List<User3>user2s;
 
-    public Address2() {
+    public Address3() {
     }
-public void addUser(User2 user2){
+public void addUser(User3 user2){
         if (user2s==null){
             user2s=new ArrayList<>();
         }
-    user2.setAddressId(this);
+    // це видалили
         user2s.add(user2);
 
 
 }
-    public Address2(String city, String street, int houseNumber, int apartmentNumber) {
+    public Address3(String city, String street, int houseNumber, int apartmentNumber) {
         this.city = city;
         this.street = street;
         this.houseNumber = houseNumber;
@@ -88,11 +88,11 @@ public void addUser(User2 user2){
         this.apartmentNumber = apartmentNumber;
     }
 
-    public List<User2> getUser2s() {
+    public List<User3> getUser2s() {
         return user2s;
     }
 
-    public void setUser2s(List<User2> user2s) {
+    public void setUser2s(List<User3> user2s) {
         this.user2s = user2s;
     }
 
