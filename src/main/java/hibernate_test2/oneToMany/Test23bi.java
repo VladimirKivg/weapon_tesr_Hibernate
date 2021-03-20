c09b44fc50656a0f0a4d79bcce3ac9f01a1d2f26
VladimirKing <cheremuha1@gmail.com>
2021-03-21 01:04:35 +0200

    mm

--- src/main/java/hibernate_test2/oneToMany/Test23bi.java
+++ src/main/java/hibernate_test2/oneToMany/Test23bi.java
@@ -2,11 +2,11 @@ package hibernate_test2.oneToMany;
 // дістаємо та видаляємо обект повністю
 import org.hibernate.Session;
 
-public class Test23 {
+public class Test23bi {
     public static void main(String[] args) {
        Session session= null;
        try {
-           session=Test22.getFactory().getCurrentSession();
+           session= Test22bi.getFactory().getCurrentSession();
            session.beginTransaction();
            Address2 address2=session.get(Address2.class,28);
            session.delete(address2);
