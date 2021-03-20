927f3640ed7d38d101ee1dc47e2f958141f7facc
VladimirKing <cheremuha1@gmail.com>
2021-03-21 01:11:05 +0200

    mm

--- src/main/java/hibernate_test2/Test21bi.java
+++ src/main/java/hibernate_test2/Test21bi.java
@@ -6,7 +6,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
-public class Test21 {
+public class Test21bi {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
