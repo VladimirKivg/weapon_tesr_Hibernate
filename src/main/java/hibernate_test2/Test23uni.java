b7b0d9d9e573832528ff15d3c2ec85dcc5a43d59
VladimirKing <cheremuha1@gmail.com>
2021-03-24 22:08:31 +0200

    mm

--- src/main/java/hibernate_test2/Test23uni.java
+++ src/main/java/hibernate_test2/Test23uni.java
@@ -8,23 +8,23 @@ import org.hibernate.Session;
 
 public class Test23uni {
     public static void main(String[] args) {
-      Session session=null;
-      try {
-          session= Test22uni.getFactory().getCurrentSession();
-          session.beginTransaction();
-          User3 user3 = session.get(User3.class,32);
+        Session session = null;
+        try {
+            session = Test22uni.getFactory().getCurrentSession();
+            session.beginTransaction();
+            User3 user3 = session.get(User3.class, 32);
 
-          System.out.println(user3);
-          session.delete(user3);
+            System.out.println(user3);
+            session.delete(user3);
 
 
-          session.getTransaction().commit();
-      } catch (Exception e) {
-          System.out.println("__________"+e);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println("__________" + e);
 
-      }finally {
-          session.close();
-      }
+        } finally {
+            session.close();
+        }
 
     }
 }
