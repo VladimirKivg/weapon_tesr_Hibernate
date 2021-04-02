d72e9b52245a51b20416a19c86152637763eedb3
VladimirKing <cheremuha1@gmail.com>
2021-04-02 11:19:10 +0300

    mm

--- src/main/java/hibernate_test2/oneToMany/Test22bi.java
+++ src/main/java/hibernate_test2/oneToMany/Test22bi.java
@@ -1,5 +1,6 @@
 package hibernate_test2.oneToMany;
 
+import hibernate_test2.oneToMany.bi.User2;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
